9a49bfa50e5beb0241f251bd2e96a94d363701d1
Francesco Gazzetta <francygazz@gmail.com>
2017-02-16 20:02:54 +0100

    some example fractals

--- fractal-generator/fractal-generator.hs
+++ fractal-generator/fractal-generator.hs
@@ -18,7 +18,9 @@ halveTrans = Trans 0.5 0 0 0.5
 
 idVect = Vect 0 0
 
-fr0 = [(halveTrans, Vect 0 1), (halveTrans, Vect (cos (-pi/6)) (-0.5)), (halveTrans, Vect (cos (pi-pi/6)) 0.5)]
+sierpinski = [(halveTrans, Vect 0 1), (halveTrans, Vect (cos (-pi/6)) (-0.5)), (halveTrans, Vect (cos (pi-pi/6)) 0.5)]
+fern = [(Trans 0 0 0 0.16, Vect 0 0), (Trans 0.85 (-0.04) 0.04 0.85, Vect 0 1.60), (Trans 0.20 0.23 (-0.26) 0.22, Vect 0 1.60), (Trans (-0.15) 0.26 0.28 0.24, Vect 0 0.44)]
+fr0 = sierpinski
 
 minDist = 0.0001
 
